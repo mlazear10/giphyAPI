0210cf438100cf150dd496ee6dbaaac973016b45
mlazear10 <mlazear10@icloud.com>
2017-08-09 16:25:20 -0700

    updated files

--- logic.js
+++ logic.js
@@ -1,75 +1,78 @@
-
-$(document).ready(function() {
-// Page loaded
- $(function() {
-  console.log("Page loaded");
-  renderButtons();
- });
-
-//Create array of food buttons
-var food = ["Mango", "bananas", "apples", "carrots", "beets", "french fries", "beans"];
-var x = food.length;
-// test to see if code is written
-console.log(x);
-
-// Function for displaying food data
-function renderButtons() {
-// function renderButtons(searchArray,classToAdd,areaToAddTo) another way to create the function.
-    $('#addButton').empty();
-    for (var i = 0; i < food.length; i++) {
-        $('#addButton').append("<button class='btn btn-successs' type='submit'>" + food[i] + "</button> ");
-        }
-    }
-
-
-$("#addFood").on("click", function(event) {
-// event.preventDefault() prevents the form from trying to submit itself.
-// We're using a form so that the user can hit enter instead of clicking the button if they w ant
- event.preventDefault();
-
- // sets the var food1 to the value in the html input field
- var food1 = $("#foodInput").val().trim();
- //this pushes the value to the array above
- food.push(food1);
- renderButtons();
- $("#foodInput").val("");
+// this loads the buttons when the page opens
+$(function(){
+ console.log("page loaded")
+ populateButtons(searchArray,'searchButton', '#buttonsArea');
 });
 
+var searchArray = ['dog', 'cat', 'bird', 'horse', 'gun'];
 
-
-// Returns the array to its original setup
-$("#reset").on('click', function() {
-food = food.slice(0, x);
-    renderButtons();
-});
-
-//set up the API to call the food giphs - need to finish this API
-function displayFoodInfo() {
-
+//function that populates the buttons
+function populateButtons(searchArray, classToAdd, areaToAddTo) {
+	$(areaToAddTo).empty();
+	for(var i=0; i<searchArray.length ;i++){
+		var a = $('<button class="btn btn-success">');
+		a.addClass(classToAdd);
+		a.attr('data-type', searchArray[i]);
+		a.text(searchArray[i]);
+		$(areaToAddTo).append(a);
+	}
 }
 
-function getApi() {
-  //console.log("a click");
-  var content = $(this).text();
-  queryURL ="http://api.giphy.com/v1/gifs/search?q="+content+"&api_key=83b6f797941e41fb940e70d7a6fa9d09&limit=10";
-         $("#gifAdd").empty();
+// I need to make sure the API information is accurate with the documentation or it will not pull the images correctly
+$(document).on('click', '.searchButton', function(){
+	 $('#searches').empty();
+	 var type = $(this).data('type');
+	 console.log(type);
+	 var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q="+type+"&limit=10";
+	 $.ajax({ url: queryURL, method: 'GET' })
+	  .done(function(response){
+	  	console.log(response);
+	 	for(var i = 0; i < response.data.length; i++){
+	 		var searchDiv = $('<div class="search-item">');
+	 		//Creates the rating variable for the text
+	 		var rating = response.data[i].rating;
+	 		var p = $('<p>').text('Rating: ' + rating);
+	 		var animated = response.data[i].images.fixed_height.url;
+	 		var still = response.data[i].images.fixed_height_still.url;
+	 		// created the image tab
+	 		var image = $('<img>');
+	 		image.attr('src', still);
+	 		image.attr('data-still', still);
+	 		//this below is equal to the animated variable
+	 		image.attr('data-animated', animated);
+	 		image.attr('data-state', 'still');
+	 		image.addClass('searchImage');
+	 		searchDiv.append(p);
+	 		searchDiv.append(image);
+	 		$('#searches').append(searchDiv);
+	 	}
+	 })
+})
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).done(function(response)
-            {
-                for (var i = 0; i < 10; i++)
-                 {        
-                console.log(response.data[i].images.original_still.url);
-                var myimg = $('<div class="thumbnail imageShown" data-status="still" data-text='+response.data[i].rating+' data-scr='+response.data[i].images.original_still.url+' value='+response.data[i].images.original.url+'><img id="myimg" src=' +response.data[i].images.original_still.url+ '> Rating: '+response.data[i].rating+'</div>' );
-                $("#gifAdd").append(myimg);
-            }
+// adjust the fixed and still states of the images
+$(document).on('click', '.searchImage', function() {
+	// data('data-state') needs to be data-state not state because 
+	var state = $(this).attr('data-state');
+	if(state == 'still') {
+		$(this).attr('src', $(this).data('animated'));
+		$(this).attr('data-state', 'animated');
+	}
+	else {
+		$(this).attr('src', $(this).data('still'));
+		$(this).attr('data-state', 'still');
+	}
+})
 
-        })
-    }
+// adds the buttons to search array
+$('#addSearch').on('click', function(){
+	var newSearch = $('input').val().trim();
+	searchArray.push(newSearch);
+	populateButtons(searchArray, 'searchButton', '#buttonsArea');
+	// we have to say retrun false to prevent it from reloading the page
+	return false;
+})
 
 
-//Closes out jQuery
-});
+
+
 
